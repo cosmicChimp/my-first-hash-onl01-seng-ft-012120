45460ce29907d0c65386783f9720323582ce2edf
Ian Marrs <ian.t.marrs@gmail.com>
2020-01-25 07:13:38 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -5,8 +5,7 @@ def my_hash = {"name" => "Ian", "age" => 29, "State" => "Colorado"}
 end
 
 
-def shipping_manifest 
-  the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+def shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
@@ -33,7 +32,7 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-                  shipping_manifest[muskets] = 2
+                  shipping_manifest["muskets"] = 2
 
 
   # add 4 gun powder to the shipping_manifest hash below
